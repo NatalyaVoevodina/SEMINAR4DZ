debd49733f3c90d1253cc649a98ae9931f2cf389
natlya_voevodina <voevodochka1988@yandex.ru>
2023-02-25 16:46:15 +0300

    task2.4

---
+++ task2.4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int n = N;
+while (n != 0)
+{
+    sum += n % 10;
+    n /= 10;
+}
+Console.WriteLine($"Сумма цифр числа {N} = {sum}");
