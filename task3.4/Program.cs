ff5d90f24f698a9149d5c767f768691a0c588051
natlya_voevodina <voevodochka1988@yandex.ru>
2023-02-25 16:48:18 +0300

    task3.4

---
+++ task3.4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int n = 8;
+int[] arr = new int[n];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(-100,100);
+}
+Console.WriteLine(string.Join(", ", arr));
